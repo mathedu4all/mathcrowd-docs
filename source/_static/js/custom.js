5a355667a63cf6f9bf193b6798d4f1d8f247361f
Charles <charles@mathcrowd.cn>
2021-08-10 16:24:53 +0800

    add custom.js

---
+++ source/_static/js/custom.js
@@ -0,0 +1,15 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2Y68KH2XE9');
+
+<!-- Hotjar Tracking Code for www.mathcrowd.cn -->
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2546197,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
--- source/conf.py
+++ source/conf.py
@@ -61,3 +61,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_js_files = [
+    'https://www.googletagmanager.com/gtag/js?id=G-2Y68KH2XE9',
+    'js/custom.js',
+]
